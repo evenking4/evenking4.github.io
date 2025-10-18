ef2e171741f1e3924ae53d85e846d0680f77481b
ppk <2770852375@qq.com>
2025-10-18 23:06:14 +0800

    Site updated: 2025-10-18 23:06:13

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1760799665722";
+const VERSION = "1760799972855";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
